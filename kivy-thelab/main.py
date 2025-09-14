d0d507d057ab31ad51ef6ef9c00edcc6931713a9
Grasjusz <gw.backlog@gmail.com>
2025-09-14 18:23:41 +0200

    Commit

--- kivy-thelab/main.py
+++ kivy-thelab/main.py
@@ -1,6 +1,6 @@
 from kivy.app import App
 from kivy.metrics import dp
-from kivy.properties import StringProperty
+from kivy.properties import StringProperty, BooleanProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
@@ -8,10 +8,28 @@ from kivy.uix.stacklayout import StackLayout
 from kivy.uix.widget import Widget
 
 class WidgetsExample(GridLayout):
-    my_text = StringProperty("Hello!!!")
+    count = 1
+    count_enabled = BooleanProperty(False)
+
+    my_text = StringProperty("1")
     def on_button_click(self):
         print("Button clicked")
-        self.my_text = "You clicked"
+        if self.count_enabled:
+            self.count += 1
+            self.my_text = str(self.count)
+
+    def on_toggle_button_state(self, widget):
+        print("toggle state: " + widget.state)
+        if widget.state == "normal":
+            widget.text = "OFF"
+            self.count_enabled = False
+        else:
+            widget.text = "ON"
+            self.count_enabled = True
+
+    def on_switch_active(self, widget):
+        print("Switch: " + str(widget.active))
+
 
 
 
